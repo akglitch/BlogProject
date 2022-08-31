1d2e87d8893d6552e05a9928fa344152ec3d385d
kaytee <https://github.com/akglitch/kayteeestate.git>
2022-08-31 16:36:29 +0000

    initial commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,14 +17,14 @@ function Hero() {
                 <img src={item.img} alt="pic"/>
             </div>
 			<div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
-				<a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracking-wider uppercase dark:text-gray-100 bgundefined">Science</a>
+				<a rel="noopener noreferrer" href="#j" className="px-3 py-2 text-xs font-semibold tracking-wider uppercase dark:text-gray-100 bgundefined">Science</a>
 				<div className="flex flex-col justify-start text-center dark:text-gray-100">
 					<span className="text-3xl font-semibold leading-none tracking-wide">28</span>
 					<span className="leading-none uppercase">Jul</span>
 				</div>
 			</div>
 			<h2 className="z-10 p-5">
-				<a rel="noopener noreferrer" href="#" className="font-medium text-md hover:underline dark:text-gray-100">Officiis mollitia dignissimos commodi optio vero animi</a>
+				<a rel="noopener noreferrer" href="#h" className="font-medium text-md hover:underline dark:text-gray-100">Officiis mollitia dignissimos commodi optio vero animi</a>
 			</h2>
 		</div>
 	</div>
